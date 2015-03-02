14959f5ca275a76872c72ec8e096af92aecf5177
Bhawan Virk <bhawanvirk@Bhawans-MacBook-Pro.local>
2015-03-02 12:42:55 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,24 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Bhawan Virk on 3/2/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
