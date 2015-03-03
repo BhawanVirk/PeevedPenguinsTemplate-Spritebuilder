181cfbc43a8fd4d84291ea4ac0c72701e15bd16a
Bhawan Virk <bhawanvirk@Bhawans-MacBook-Pro.local>
2015-03-03 09:32:17 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -16,7 +16,9 @@
     CCNode *_contentNode;
     CCNode *_pullbackNode;
     CCNode *_mouseJointNode;
+    CCNode *_currentPenguin;
     CCPhysicsJoint *_mouseJoint;
+    CCPhysicsJoint *_penguinCatapultJoint;
 }
 
 
@@ -51,6 +53,20 @@
         _mouseJointNode.position = touchLocation;
         // setup a spring joint between mouseJointNode and catapultArm
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
+        
+        // create a penguin from the ccb-file
+        _currentPenguin = [CCBReader load:@"Penguin"];
+        // initially position it on the scoop, 34,138 is the position in the node space of the _catapultArm
+        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
+        // transform the world position to the node space to which the penguin will be added (_physicsNode)
+        _currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
+        // add it to the physics world
+        [_physicsNode addChild:_currentPenguin];
+        // we don't want the penguin to rotate in the scoop
+        _currentPenguin.physicsBody.allowsRotation = NO;
+        
+        // create a joint to keep the penguin fixed to the scoop until the catapult is released
+        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
     }
 }
 
