3425a7efddc2ada16212f535f06c85bb344b38fb
Bhawan Virk <bhawanvirk@Bhawans-MacBook-Pro.local>
2015-03-02 17:13:32 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -54,7 +54,7 @@
     [self runAction:follow];
     
     // make retry button stick to the top left corner
-    CCActionMoveTo *retryFollow = [CCActionMoveTo actionWithDuration:1.0 position:ccpAdd(penguin.position, ccp(0, -100))];
+    CCActionMoveTo *retryFollow = [CCActionMoveTo actionWithDuration:1.0 position:ccp(penguin.position.x, _retryButton.position.y)];
     [_retryButton runAction:retryFollow];
 }
 
