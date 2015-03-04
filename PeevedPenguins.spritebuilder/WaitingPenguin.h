9d8165f5bd23b762aebaef5e7b18c5dd67f731d3
Bhawan Virk <bhawanvirk@Bhawans-MacBook-Pro.local>
2015-03-03 21:01:52 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by Bhawan Virk on 3/3/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
